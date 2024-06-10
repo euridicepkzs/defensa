1ab53eb929a47dc192c36860f9e354c18ba7085e
euridicepkzs <165715646+euridicepkzs@users.noreply.github.com>
2024-06-09 23:53:47 -0400

    Delete pob.m

--- pob.m
+++
@@ -1,8 +0,0 @@
-
-P0 = 50000000;
-d = 20;
-t = [5, 10, 15];
-P = P0 * 2.^(t/d);
-fprintf('La población dentro de 5 años será: %.2f millones\n', P(1) / 1e6);
-fprintf('La población dentro de 10 años será: %.2f millones\n', P(2) / 1e6);
-fprintf('La población dentro de 15 años será: %.2f millones\n', P(3) / 1e6);
